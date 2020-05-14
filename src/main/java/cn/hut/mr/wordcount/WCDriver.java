82ff253112ece456b39271fa8f9af64996c7d427
xpyuanfly <47739070@qq.com>
2020-05-14 15:25:24 +0800

    commit

--- src/main/java/cn/hut/mr/wordcount/WCDriver.java
+++ src/main/java/cn/hut/mr/wordcount/WCDriver.java
@@ -1,6 +1,9 @@
 package cn.hut.mr.wordcount;
 
+import java.io.IOException;
+
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -10,13 +13,22 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class WCDriver {
 
-    public static void main(String[] args) {    
+    public static void main(String[] args) throws IOException {
         // String arg1 = args[0];
         // String arg2 = args[1];
+        Configuration conf =new Configuration();
         String arg1 = "d://centos//input";
         String arg2 = "d://centos//output";
+
+        Path outPath = new Path(arg2);
+        // 判断输出文件是否已经存在，存在的话就将其删除
+        FileSystem fs = FileSystem.get(conf);
+        if (fs.exists(outPath)) {
+            fs.delete(outPath, true);
+        }
+
         try {
-            Job job = Job.getInstance(new Configuration());
+            Job job = Job.getInstance(conf);
             // 设置类路径
             job.setJarByClass(WCDriver.class);
             // 设置Mapper和Reducer
